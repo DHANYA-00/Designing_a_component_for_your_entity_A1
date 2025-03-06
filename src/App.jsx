89efa8675d361537d3a1c5753d87e5e88ebc5bec
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-03-06 14:47:09 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,49 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Usercard from './components/usercard.jsx';
+import img from "./assets/Avatar.png"
+
+
 
 function App() {
-  const cards = Array.from({ length: 10 });
+  const cards=[
+    {
+      name:"Shankar",
+      email:"shankar@gamil.com",
+      phonenumber:12345678,
+      address:"36, gandhi street, chennai",
+      image:img,
+    },
+    {
+      name:"Abishek",
+      email:"abishek@gamil.com",
+      phonenumber:12345678,
+      address:"36, Nehru street, chennai",
+      image:img,
+    },
+    {
+      name:"Lithish",
+      email:"lithish@gamil.com",
+      phonenumber:12345678,
+      address:"36,  mosque street, chennai",
+      image:img,
+    },
+    {
+      name:"Rohan",
+      email:"rohan@gamil.com",
+      phonenumber:12345678,
+      address:"36, dark street, chennai",
+      image:img,
+    },
+    {
+      name:"Joe Celaster",
+      email:"celaster@gamil.com",
+      phonenumber:12345678,
+      address:"36, Church street street, chennai",
+      image:img,
+    },
+  ]
+
   const gridStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(4, 1fr)',
@@ -15,8 +55,8 @@ function App() {
 
   return (
     <div style={gridStyle}>
-      {cards.map((_, index) => (
-        <Usercard key={index} />
+      {cards.map((i, index) => (
+        <Usercard key={index} name={i.name} email={i.email} phonenumber={i.phonenumber} address={i.address} image={i.image}/>
       ))}
     </div>
   );
--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,35 @@
 // write the component code here
+import React from 'react'
+import "../App.css"
+
+
+function usercard({name,email,phonenumber,address,image,style}) {
+  return (
+        <div className='box'>
+
+            <div className='profile'>
+                <img src={image} alt="profile image" />
+            </div>
+
+            <div className='name'>
+                <p><strong>Name:</strong>{name}</p>
+            </div>
+
+            <div className='email'>
+                <p><strong>Email:</strong>{email}</p>
+            </div>
+
+            <div className='phno'>
+                <p><strong>Phone number:</strong>{phonenumber}</p>
+            </div>
+
+            <div className='address'>
+                <p><strong>Address:</strong>{address}</p>
+            </div>
+
+        </div>
+  );
+}
+
+
+export default usercard;
